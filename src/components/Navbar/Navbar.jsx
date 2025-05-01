4d9a09447de4e2efcd51fcbb1b559a0617168266
Belal1khalil <belalkhalil116@gmail.com>
2025-05-01 15:38:27 +0200

    make navbar more responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -317,7 +317,7 @@ export default function Navbar() {
   return (
     <>
       <nav className="bg-slate-200 shadow-sm py-3 px-4 fixed top-0 right-0 left-0 z-50">
-        <div className="container flex flex-wrap  justify-between">
+        <div className="container flex flex-nowrap  justify-between space-x-3">
           <div className="flex items-center gap-12">
             <a href="/">
               <img src={Logo} alt="FreshCart Logo"  />
